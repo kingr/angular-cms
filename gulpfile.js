200c5773a7ad8a51f0aac6bd74f0e9f92b4f2dfc
Kurt Farao <kefarao@gmail.com>
2015-08-12 23:55:21 +0200

    add gulp watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 
-gulp.task('default', ['less']);
+// call watch function
+gulp.task('default', ['watch']);
 
+// define less compiler
 gulp.task('less', function () {
   return gulp.src('app/css/less/app.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('app/css/'));
+});
+
+// define watcher
+gulp.task('watch', function(){
+	gulp.watch('app/css/less/**/*.less', ['less'])
 });
