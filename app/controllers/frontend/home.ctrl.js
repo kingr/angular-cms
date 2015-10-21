c4203bccf0e2e9378af505613f6e5419f4e09aa1
Kurt Farao <kefarao@gmail.com>
2015-10-21 15:20:39 +0200

    make home page content dynamic

--- app/controllers/frontend/home.ctrl.js
+++ app/controllers/frontend/home.ctrl.js
@@ -4,12 +4,18 @@
 
 	var home = angular.module('home.ctrl', []);
 
-	home.controller('home.ctrl', ['$scope', 
-    	function($scope) {
+	home.controller('home.ctrl', ['$scope','$firebaseObject', '$state' ,'firebaseInit',
+    	function($scope, $firebaseObject, $state, firebaseInit) {
 
-    		this.title = "Welcome to my blog! Here I document the process of converting a Hyosung GT250R to pure electric. The goal is also to push the envolope of technology and pack the bike with extremely advanced features.";
-    		this.slides = ["im one","im two",];
-    		this.name = "It is common to attach Controllers at different levels of the DOM hierarchy. Since the ng-controller directive creates a new child scope, we get a hierarchy of scopes that inherit from each other. The $scope that each Controller receives will have access to properties and methods defined by Controllers higher up the hierarchy. See Understanding Scopes for more information about scope inheritance.";
+    		// open conn
+    	   var ref = firebaseInit;
+
+         	// define Intro data object
+			var obj = $firebaseObject(ref.child("home").child('intro'));
+
+         	// add three-way data binding
+			obj.$bindTo($scope, "intro");
+
   		}]
 	);
 	
