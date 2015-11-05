7df468abf8b0e74ffd206311a41f07b7fbcaf2c9
Kurt Farao <kefarao@gmail.com>
2015-11-05 20:46:42 +0200

    media gallery

--- app/directives/mediaGalleryConstructor.js
+++ app/directives/mediaGalleryConstructor.js
@@ -6,9 +6,38 @@
 
 	mediaGalleryContructor.directive('mediaGalleryContructor',function() {
 	  
-	    
+	  	function link(scope, element){
+	  		
+	  		var data = scope.data;
+
+	  		var container = element;
+
+			var createImg = function(src){
+
+				// contruct img tag
+				var img = document.createElement("img");
+				img.src = 'uploads/' + src;
+
+				// append image to container
+				container.appendChild(img);
+
+			};
+
+			for(var i=0; i < data.length; i++) {
+
+				// create images
+				createImg(data[i]);
+
+			}
+
+	  	}
+
 	    return {
-	    	template: "hello im a directive {{ home.name }}"
+	    	restrict : 'E',
+	    	scope: {
+	    		data: '='
+	    	},
+			link: link	    	
 	    }
 	    
 	 });
